7eb06f7f1c2e3329a0ef03fc55c839e33fee991b
rgarcia2304 <garcia2304@icloud.com>
2026-08-20 16:26:23 +0000

    was able to create a new namespace for my program

--- mini-docker.cpp
+++ mini-docker.cpp
@@ -2,27 +2,46 @@
 #include <unistd.h>
 #include<sys/wait.h>
 #include <cassert> 
+#include <cstdlib>
+#include <string> 
+#include <vector> 
+
+constexpr size_t STACK_SIZE = 1024 * 1024; 
+
+int child_main(void* arg)
+{
+	char** args = static_cast<char**>(arg); 
+	std::cout << getpid(); 
+	execv(args[0], args);
+	return 1; 
+}
+
 
 int main(int argc, char* argv[])
 {
-	
-	pid_t pid = fork(); 
+	std::vector<char> stack(STACK_SIZE); 
 
-	if(pid < 0)
+	char *stack_top = stack.data() + STACK_SIZE; 
+
+	pid_t pid = clone(child_main, stack_top, CLONE_NEWPID | SIGCHLD, static_cast<void*>(argv + 1)); 
+
+
+	if (pid == -1)
 	{
-		std::cerr << "Fork failed";
-		return -1; 
-	} else if (pid == 0)
-	{
-		execv(argv[1], argv + 1); 
-		std::perror("execv failed");
-		return -1; 
-	} else
-	{
-		int status; 
-		pid_t exited_pid = waitpid(pid, &status, 0); 
-		assert(exited_pid == pid); 
+		std::perror("Clone Failed"); 
+		return EXIT_FAILURE; 
 	}
 
-	return 0; 	
+	int status; 
+	if (waitpid(pid, &status, 0) == -1)
+	{
+		std::perror("waitpid failed"); 
+	}
+
+	if (WIFEXITED(status))
+	{
+		std::cout << "child exited with code " << WEXITSTATUS(status) << '\n'; 
+	}
+
+	return EXIT_SUCCESS; 
 }
